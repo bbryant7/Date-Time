19ca80f530bae89fdb3d61b6adc5924e4608452b
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-14 15:03:55 -0500

    add Date and time, leap year, and DLS

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-console.log("banana"
-);
 
 const moment = require('moment');
 const chalk = require('chalk');
+let now = moment();
+let leapYear = now.isLeapYear();
+let dst = now.isDST();
+
+// print out the date and time info
+console.log(now);
+console.log("It is " + chalk.blue(now.format('dddd, MMMM Do YYYY, h:mm:ss a')));
 
 
-// var now = moment();
-// console.log(now);
-// let day = moment("1995-12-25");
-//  console.log(day);
+// print out leap year info (true/false)
+if (leapYear === true){
+  console.log("It " + chalk.green("is") + " a leap Year!");
+} else {
+  console.log("It " + chalk.red("is not") + " a leap Year!");
+}
 
 
-
-
- let date = new Date(2010, 1, 14, 15, 25, 50);
-let wrapped = moment(date);
-console.log("It is " + chalk.blue(wrapped.format('dddd, MMMM Do YYYY, h:mm:ss a')));
+// print out DST info (true/false)
+if (dst === true){
+  console.log("It " + chalk.green("is") + " during Day-Light Savings!");
+} else {
+  console.log("It " + chalk.red("is not") + " during Day-Light Savings!");
+}
